bf15bef3cd78a5b4dbff60083b76bdc9ed42771c
ZacharyWDWS <zwhitty11@gmail.com>
2024-05-29 22:07:48 -0500

    Got basic card working

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations'
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ButtonComponent } from './shared/button/button.component'; 
+import { CardComponent } from './shared/card/card.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { ToolbarModule } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar'; 
 import { ButtonModule } from 'primeng/button'; 
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -30,8 +33,11 @@ import { SplitButtonModule } from 'primeng/splitbutton';
     ButtonModule,
     SplitButtonModule,
     ButtonComponent,
+    CardModule,
+    CardComponent,
   ],
-
+  exports: [
+  ],
   providers: [
     provideAnimations()
   ],
