e7e94d832fa9150fcbafb7c09c9b709ca655ec56
derekdreibrodt <taylorbendtransport@wdwebsolutions-email.com>
2024-05-30 23:40:47 -0500

    Added routing values and added contact page

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  lastScrollTop = 0;
+  navbarHidden = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScrollTop > this.lastScrollTop) {
+      // Scrolling down
+      this.navbarHidden = true;
+    } else {
+      // Scrolling up
+      this.navbarHidden = false;
+    }
+    this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
+  }
 }
