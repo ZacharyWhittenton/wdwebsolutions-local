8f2ce432d036da1234a36121000501b26e302980
derekdreibrodt <taylorbendtransport@wdwebsolutions-email.com>
2024-05-30 23:27:26 -0500

    Updated homepage to have basic hero section

--- client/src/app/home/home-hero/home-hero.component.ts
+++ client/src/app/home/home-hero/home-hero.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HomeHeroComponent {
 
+  scrollToNextElement() {
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: 'smooth'
+    });
+  }
 }
