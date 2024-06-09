20df52e7917fb49ebe2e4975f604bdaa3bbf9b31
derekdreibrodt <taylorbendtransport@wdwebsolutions-email.com>
2024-06-09 12:19:24 -0500

    made some aesthetic changes for wedding presentation lol

--- client/src/app/home/home-hero/home-hero.component.ts
+++ client/src/app/home/home-hero/home-hero.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { NextElementPointerComponent } from "../../shared/next-element-pointer/next-element-pointer.component";
-
+import { RouterModule } from '@angular/router';
 @Component({
     selector: 'app-home-hero',
     standalone: true,
     templateUrl: './home-hero.component.html',
     styleUrl: './home-hero.component.scss',
-    imports: [NextElementPointerComponent]
+    imports: [NextElementPointerComponent, RouterModule]
 })
 export class HomeHeroComponent {
 
